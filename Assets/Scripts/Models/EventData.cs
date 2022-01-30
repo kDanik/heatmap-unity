708d780869f5656879770720bd265b89285e16df
Daniil Kurachkin <86569730+kDanik@users.noreply.github.com>
2022-01-29 16:10:23 -0800

    Rename Postion to EventPosition

--- Assets/Scripts/Models/EventData.cs
+++ Assets/Scripts/Models/EventData.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // EventData is collection of tracked positions with specific tag.
-// For example it could be "Logouts","Movement" and anything else
 
 public class EventData
 {
-    public List<Position> positions = new List<Position>();
+    public List<EventPosition> positions = new List<EventPosition>();
 
     public string name;
     public bool active = false;
